c8287d22fd4f0c56c7710de569676f3f932d293d
Rishabh Tyagi <30928799+ristyagi@users.noreply.github.com>
2024-07-31 20:57:13 +0530

    Create 0_Cheat_Sheet.py

---
+++ 0_Cheat_Sheet.py
@@ -0,0 +1,3 @@
+import pandas as pd
+pd.set_option('display.max_columns', None) # pandas setting to force terminal to display all columns of dataframe
+pd.set_option('display.max_rows', None) # pandas setting to force terminal to display all rows of dataframe
