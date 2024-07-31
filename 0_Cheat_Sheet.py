dfd0e0c91700eccaaacfc0895e0ffd2d5d1ec13c
Rishabh Tyagi <30928799+ristyagi@users.noreply.github.com>
2024-07-31 23:39:35 +0530

    Update 0_Cheat_Sheet.py

--- 0_Cheat_Sheet.py
+++ 0_Cheat_Sheet.py
@@ -1,3 +1,7 @@
 import pandas as pd
 pd.set_option('display.max_columns', None) # pandas setting to force terminal to display all columns of dataframe
 pd.set_option('display.max_rows', None) # pandas setting to force terminal to display all rows of dataframe
+
+## ATTRIBUTE is a piece of data that belongs to the object, it simply stores a data/characteristic about the object, like df.shape, its a tuple having info of dataframe shape
+## METHOD on the other hand is a command or a behaviour that we ask the the object to perform for us.Like Head() method
+## ARGUMENT
