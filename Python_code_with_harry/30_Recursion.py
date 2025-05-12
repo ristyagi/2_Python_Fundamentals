263a86dbfe973ad3dc513fc0d576ac61133da911
Rishabh Tyagi <30928799+ristyagi@users.noreply.github.com>
2025-05-12 12:00:58 +0530

    Create 30_Recursion.py

---
+++ Python_code_with_harry/30_Recursion.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  7 21:47:33 2025
+
+@author: Rishabh_Tyagi
+"""
+
+"""
+https://www.youtube.com/watch?v=XYwJKFB8DUk&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&index=30
+
+Recursion in Python | Python Tutorial - Day #30
+
+"""
+
+
+"""
+calling a function inside a function is called recursion
+
+example where recursion can be used is factorial
+
+
+factorial(6) = 6*5*4*3*2*1
+factorial(5) = 5*4*3*2*1
+factorial(0) = 1
+
+this can be done like 
+factorial(n) = n * factorial(n-1)
+
+factorial(6) = 6*factorial(5)
+factorial(5) = 5*factorial(4)..and do on
+"""
+
+def factorial(n):
+    '''
+    Parameters
+    ----------
+    n : integer
+        value whose factorial needs to be calculated.
+
+    Returns
+    -------
+    factorial of n.
+
+    '''
+    
+    if (n==0 or n==1):
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+factorial(0)
+factorial(1)
+factorial(4)
+
+# this is how recursion will work
+# factorial(4)
+# 4 * factorial(3)
+# 4 * 3 * factorial(2)
+# 4 * 3 * 2* factorial(1)
+    
+
+print(factorial.__doc__)
+
+
+
+# calculation of fibonacci series
+
+# f(0) = 0
+# f(1) = 1
+# f(2) = 1
+# f(3) = 2
+# f(n) = f(n-1) + f(n-2)
+
+def fib(n):
+    if (n==0):
+        return 0
+    elif (n==1):
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+fib(0)
+fib(1)    
+fib(2)
+fib(3)
+fib(4)
+fib(5)
